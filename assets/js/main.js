22bfa9dcafc335fba45289188a29cff26cc4737d
antonimoferraz <Antonimo@AntonioFerraz.local>
2016-05-17 23:58:21 +0100

    closing design
    closing design rules. putting elements together

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+
+function mobileNav() {
+  $('.mobile-nav-toggle').on('click', function(){
+    var status = $(this).hasClass('is-open');
+    if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
+    else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
+  });
+}
+
+
+
+ mobileNav();
+  });
